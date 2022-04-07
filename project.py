57fc539255706753eb3d959abd5afc5bafc3207c
ali <alibelyazid@gmail.com>
2022-04-07 16:38:19 +0200

    (Vrai FIN) Solve the problem with glpk

--- project.py
+++ project.py
@@ -18,8 +18,9 @@ model.x = pyo.Var(model.P,model.B, domain=pyo.NonNegativeReals, bounds=(0,1))
 model.y = pyo.Var(model.B, domain=pyo.NonNegativeReals, bounds=(0,1))
 
 """Récuperation des données à input dans le modèle"""
-FILE = './Instances/bin_pack_20_2.dat'
+FILE = './Instances/bin_pack_60_2.dat'
 data.load(filename=FILE)
+
 
 """Etablissement fonction objective"""
 def obj_expression(m):
@@ -36,15 +37,49 @@ def sx_constraint_rule(m, b):
 model.sxConstraint = pyo.Constraint(model.P, rule=sx_constraint_rule)
 
 """Affichage des résultats"""
-
 instance = model.create_instance(data)
 opt = pyo.SolverFactory('glpk')
 result = opt.solve(instance)
 
 print(result)
+    
+"""Affichage des variable x pour lesqueslles on a un résultat > 0"""
+#for i in instance.x:
+#    if pyo.value(instance.x[i]) > 0:
+#        print(instance.x[i], "de valeur : ", pyo.value(instance.x[i]))
 
-"""Affichage des variable x pour lesqueslles on a un résultat"""
+"""Affichage des boites utilisées"""
+#for i in instance.y:
+#    if pyo.value(instance.y[i]) > 0:
+#        print(instance.y[i], "=", pyo.value(instance.y[i]))
 
-for i in instance.x:
-    if pyo.value(instance.x[i]) > 0:
-        print(instance.x[i], "de valeur : ", pyo.value(instance.x[i]))
+"""Méthode heuristique : First Fit Decreasing"""
+def FirstFit(I, cap, size):
+    bins = [cap]
+    for i in range(len(size)):
+        iFit = False
+        for j in range(len(bins)):
+            if size[i] <= bins[j]:
+                bins[j] = bins[j] - size[i]
+                break
+            if j == len(bins)-1:
+                iFit = True
+
+        if iFit is True:
+            bins.append(cap)
+            bins[len(bins)-1] -= size[i]
+
+    return len(bins)
+
+def FF_decreasing_heuristic(I, cap, size):
+    sizeSorted = size.copy()
+    sizeSorted.sort(reverse = True)
+    return FirstFit(I, cap, sizeSorted)
+
+n = 60
+c = 150
+w = []
+for i in instance.size:
+    w.append(instance.size[i])
+
+print(FF_decreasing_heuristic(n,c,w)) 
